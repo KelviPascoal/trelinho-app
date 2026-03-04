69c562d3c0aa643c1644f7f0668b0b62f3a11e5e
kelvips <kelvi.pascoal@outlook.com>
2026-03-03 22:20:56 -0300

    chore: cria package de identação geral

---
+++ trelinho-config/eslint/frontend.js
@@ -0,0 +1,39 @@
+import js from '@eslint/js';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import { defineConfig, globalIgnores } from 'eslint/config';
+
+export function frontendEslintConfig() {
+  return defineConfig([
+    globalIgnores(['dist']),
+    {
+      files: ['**/*.{ts,tsx}'],
+      extends: [
+        js.configs.recommended,
+        ...tseslint.configs.recommended,
+        reactHooks.configs.flat.recommended,
+        reactRefresh.configs.vite,
+      ],
+      languageOptions: {
+        ecmaVersion: 2020,
+        globals: globals.browser,
+      },
+    },
+    eslintPluginPrettierRecommended,
+    {
+      rules: {
+        'prettier/prettier': [
+          'error',
+          {
+            endOfLine: 'auto',
+            tabWidth: 2,
+            useTabs: false,
+          },
+        ],
+      },
+    },
+  ]);
+}
---
+++ trelinho-config/prettier.config.js
@@ -0,0 +1,11 @@
+const config = {
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  tabWidth: 2,
+  useTabs: false,
+  printWidth: 100,
+  endOfLine: 'auto',
+};
+
+export default config;
